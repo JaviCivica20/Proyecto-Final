78bca943598274d15d2abbe7e2eb330cd9c0b600
Javi Martín <alumno20civica@gmail.com>
2024-06-14 23:44:34 +0000

    nuevos tests

--- macros/yaml_generator.sql
+++ macros/yaml_generator.sql
@@ -1,3 +1,5 @@
+-- Genera el contenido del yml correspondiente a los modelos del directorio y prefijo especificados
+
 {% set models_to_generate = codegen.get_models(directory='marts/datamarts', prefix='datamart_') %}
 {{ codegen.generate_model_yaml(
     model_names = models_to_generate
--- tests/correct_staff_email.sql
+++ tests/correct_staff_email.sql
@@ -1,3 +1,5 @@
+-- Comprueba que el email tiene el formato correcto
+
 SELECT email 
 FROM {{ ref('stg_dbt_proyecto_final__staff') }}
 WHERE email NOT LIKE '%@%.%'
