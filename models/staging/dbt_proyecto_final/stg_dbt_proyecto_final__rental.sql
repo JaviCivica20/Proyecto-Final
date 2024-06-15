d9514e6b735224fdfe0d6b0042f5921037392aa1
Javi Martín <alumno20civica@gmail.com>
2024-06-15 13:01:09 +0000

    incremental casi hecho

--- models/staging/dbt_proyecto_final/stg_dbt_proyecto_final__rental.sql
+++ models/staging/dbt_proyecto_final/stg_dbt_proyecto_final__rental.sql
@@ -1,7 +1,6 @@
 {{
   config(
     materialized='incremental',
-    unique_key='rental_id',
   )
 }}
 
@@ -9,12 +8,24 @@ with base_payment as (
     select *,
         _fivetran_synced as payment_synced
     from {{ref('base_dbt_proyecto_final__payment')}}
+
+{% if is_incremental() %}
+
+	where payment_synced > (select max(_fivetran_synced) from {{ this }})
+
+{% endif %}
 ),
 
 base_rental as (
     select *,
         _fivetran_synced as rental_synced
     from {{ref('base_dbt_proyecto_final__rental')}}
+
+{% if is_incremental() %}
+
+	where rental_synced > (select max(_fivetran_synced) from {{ this }})
+
+{% endif %}
 ),
 
 final as (
@@ -30,16 +41,13 @@ final as (
         p.amount,
         p.payment_date,
         {{ add_returned_column('PAYMENT_DATE') }},
-        greatest(r.rental_synced, p.payment_synced) as _fivetran_synced
+        greatest(rental_synced, payment_synced) as _fivetran_synced
+        --greatest(r.rental_synced, p.payment_synced) as _fivetran_synced
     from base_rental r
     left join base_payment p
     on p.rental_id = r.rental_id
     
-{% if is_incremental() %}
 
-	where greatest(r.rental_synced, p.payment_synced) > (select max(_fivetran_synced) from {{ this }})
-
-{% endif %}
     
 )
 
