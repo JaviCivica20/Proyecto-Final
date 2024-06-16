3455fc3e0e25a561b399b4c0fcf8c0f6e4c7ad5b
Javi Martín <alumno20civica@gmail.com>
2024-06-16 21:04:38 +0000

    documentacion

--- models/marts/statistics/film_stock.sql
+++ models/marts/statistics/film_stock.sql
@@ -1,16 +1,17 @@
 with fct_purchase as (
     select * 
-    from {{ref('fct_purchases')}}
+    from {{ref('stg_dbt_proyecto_final__purchases')}}
 ),
 
 final as (
     select
         store_id,
         film_id,
+        current_date as stock_today,
         count(film_id) as film_stock
     from fct_purchase
-    group by 1, 2
-    order by 1, 2
+    group by 1, 2, 3
+    order by 1, 2, 3
 )
 
 select * from final 
