c41e21ee5e50834eeb1f545761f4844855666b42
Javi Martín <alumno20civica@gmail.com>
2024-06-17 09:58:31 +0000

    documentacion

--- tests/rental_date_payment_date.sql
+++ tests/rental_date_payment_date.sql
@@ -1,7 +1,7 @@
 -- Comprueba que la fecha de devolución de una película no sea anterior a la de su pago (porque se paga cuando se devuelve)
 
 SELECT *
-FROM {{ ref('stg_dbt_proyecto_final__rental') }}
-LEFT JOIN {{ ref('stg_dbt_proyecto_final__payment') }}
+FROM {{ ref('base_dbt_proyecto_final__rental') }}
+LEFT JOIN {{ ref('base_dbt_proyecto_final__payment') }}
 USING(rental_id)
 WHERE  rental_date > payment_date
