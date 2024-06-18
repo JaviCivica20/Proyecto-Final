520993ce10751b7944a955c2c4438a78caff5d77
Javi Martín <alumno20civica@gmail.com>
2024-06-18 16:02:25 +0000

    cambios de yml

--- models/marts/core/dim_film.sql
+++ models/marts/core/dim_film.sql
@@ -1,10 +1,26 @@
 with int_actors_films as (
-    select *
+    select distinct
+        film_id,
+        title,
+        description,
+        length_minutes,
+        release_year,
+        original_language,
+        max_rental_days,
+        rental_rate_price,
+        replacement_cost,
+        age_rating,
+        trailers,
+        commentaries,
+        behind_the_scenes,
+        deleted_scenes,
+        category,
+        date_load
     from {{ref('int_actors_films')}}
 ),
 
 final as (
-    select distinct
+    select
         film_id,
         title,
         description,
