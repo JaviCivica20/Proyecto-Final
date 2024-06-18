a5ce309880a8f768de7afdd3f4d8158dc12da7ce
Javi Martín <alumno20civica@gmail.com>
2024-06-18 22:51:30 +0000

    Changes

--- tests/check_correct_amount.sql
+++ tests/check_correct_amount.sql
@@ -2,13 +2,10 @@
 
 SELECT 
     amount,
-    rental_rate_price,
-    rental_date,
-    payment_date,
     case when datediff(day, rental_date, payment_date) < 1 then rental_rate_price
     else datediff(day, rental_date, payment_date) * rental_rate_price 
     end as correct_amount
-FROM {{ ref('base_dbt_proyecto_final__payment') }} p
-JOIN {{ ref('base_dbt_proyecto_final__rental') }} r using(rental_id)
+FROM {{ ref('stg_dbt_proyecto_final__payment') }} p
+RIGHT JOIN {{ ref('stg_dbt_proyecto_final__rental') }} r using(rental_id)
 JOIN {{ ref('stg_dbt_proyecto_final__film')}} f USING(film_id)
 WHERE  amount != correct_amount
