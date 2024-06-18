150bc1e9095eb407887916b7ada64c2fbdbcc742
Javi Martín <alumno20civica@gmail.com>
2024-06-18 16:29:54 +0000

    commit

--- models/marts/core/dim_film.sql
+++ models/marts/core/dim_film.sql
@@ -15,29 +15,9 @@ with int_actors_films as (
         behind_the_scenes,
         deleted_scenes,
         category,
-        date_load
+        coalesce(date_load, current_time) as date_load
     from {{ref('int_actors_films')}}
-),
-
-final as (
-    select
-        film_id,
-        title,
-        description,
-        length_minutes,
-        release_year,
-        original_language,
-        max_rental_days,
-        rental_rate_price,
-        replacement_cost,
-        age_rating,
-        trailers,
-        commentaries,
-        behind_the_scenes,
-        deleted_scenes,
-        category,
-        date_load
-    from int_actors_films
 )
 
-select * from final 
+
+select * from int_actors_films where date_load is null
